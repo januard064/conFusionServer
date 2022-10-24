66b39b7001f5dafccb5e58a0f9d729627e9d0258
Januard Lumbangaol <januargaol48@gmail.com>
2022-10-24 22:01:24 +0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey':'12345-67890-09876-54321',
-    'mongoUrl':'mongodb://localhost:27017/conFusion'
+    'mongoUrl':'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        'clientId':'769806827447262',
+        'clientSecret':'7e82ba055fa30e346995ddde4e1ef83d'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -8,33 +8,33 @@ var passport = require('passport')
 
 const User = require('../models/user')
 
-var  authenticate = require('../authenticate')
+var authenticate = require('../authenticate')
 
 router.use(bodyParser.json())
 
-router.post('/signup',cors.corsWithOptions, (req, res, next) => {
-  User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
-    if(err){
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
+  User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+    if (err) {
       res.statusCode = 500
       res.setHeader('Content-Type', 'application/json')
-      res.json({err: err})
-    } 
+      res.json({ err: err })
+    }
     else {
-      if(req.body.firstname)
+      if (req.body.firstname)
         user.firstname = req.body.firstname
-      if(req.body.lastname)
+      if (req.body.lastname)
         user.lastname = req.body.lastname
       user.save((err, user) => {
-        if(err){
+        if (err) {
           res.statusCode = 500
           res.setHeader('Content-Type', 'application/json')
-          res.json({err: err})
-          return ;
+          res.json({ err: err })
+          return;
         }
-        passport.authenticate('local')(req, res, ()=> {
+        passport.authenticate('local')(req, res, () => {
           res.statusCode = 200
           res.setHeader('Content-Type', 'application/json')
-          res.json({success: true, status: 'Registration Successful!'})
+          res.json({ success: true, status: 'Registration Successful!' })
         })
       })
     }
@@ -42,21 +42,21 @@ router.post('/signup',cors.corsWithOptions, (req, res, next) => {
 })
 
 
-router.post('/login', cors.corsWithOptions, passport.authenticate('local') ,(req, res) => {
-  
-  var token = authenticate.getToken({_id: req.user._id})
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
+
+  var token = authenticate.getToken({ _id: req.user._id })
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
-  res.json({success: true, token: token ,status: 'You are sucessefuly login!'})
+  res.json({ success: true, token: token, status: 'You are sucessefuly login!' })
 })
 
-router.get('/logout', cors.corsWithOptions, (req, res, next) =>  {
-  if(req.session){
+router.get('/logout', cors.corsWithOptions, (req, res, next) => {
+  if (req.session) {
     req.session.destroy()
     res.clearCookie('session-id')
     res.redirect('/')
   }
-  else{
+  else {
     var err = new Error(`You are not loged in`)
     err.status = 403
     next(err)
@@ -65,13 +65,22 @@ router.get('/logout', cors.corsWithOptions, (req, res, next) =>  {
 
 router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   User.find({})
-  .then((users) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.json(users)
-  },(err) => next(err))
-  .catch((err) => next(err))
+    .then((users) => {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json')
+      res.json(users)
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  }
+});
 
 module.exports = router;
